8af12ff27b5613825f165a5b7908680e2478760f
Juan Cortes Ross <juan@bohem.io>
2020-12-16 13:29:02 +0100

    sync new connections hacky

--- front/socket.js
+++ front/socket.js
@@ -1,15 +1,19 @@
 const WebSocket = require("ws");
 
 const wss = new WebSocket.Server({ port: 3002 });
-
+const queued = [];
 wss.on("connection", (ws) => {
   ws.on("message", (data) => {
     try {
       const parseAction = JSON.parse(data);
+      queued.push(data);
       wss.broadcast(data); // if it's a valid json, send it, it will be parsed by clients
     } catch (e) {}
   });
-  ws.send("Hello! Message From Server!!");
+  if (queued.length) {
+    // Could probably be done in a single message
+    for (let i = 0; i < queued.length; i++) ws.send(queued[i]);
+  }
 });
 
 wss.broadcast = function broadcast(msg) {
